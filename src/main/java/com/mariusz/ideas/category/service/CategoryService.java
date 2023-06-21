3d2dc7168e3c1132b479f1a949d2b103d146d6dd
@thiankiann <thiankiann@gmail.com>
2023-06-21 13:04:03 +0100

    prototype of Categories  implemented on all levels - controller, service, view and template ( template.html , question/single.html)

---
+++ src/main/java/com/mariusz/ideas/category/service/CategoryService.java
@@ -0,0 +1,35 @@
+package com.mariusz.ideas.category.service;
+
+import com.mariusz.ideas.guestion.domain.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class CategoryService {
+    public List<Category> getCategories() {
+        return Arrays.asList(
+                new Category("Category 1"),
+                new Category("Category 2"),
+                new Category("Category 3")
+        );
+    }
+
+    public Category getCategory(UUID id) {
+        return new Category("Category "+  id);
+    }
+
+    public Category createCategory(Category category) {
+        category.setId(UUID.randomUUID());
+        return category;
+    }
+
+    public Category updateCategory(UUID id, Category category) {
+        return category;
+    }
+
+    public void deleteCategory(UUID id) {
+    }
+}
--- src/main/java/com/mariusz/ideas/guestion/controller/QuestionViewController.java
+++ src/main/java/com/mariusz/ideas/guestion/controller/QuestionViewController.java
@@ -1,5 +1,6 @@
 package com.mariusz.ideas.guestion.controller;
 
+import com.mariusz.ideas.category.service.CategoryService;
 import com.mariusz.ideas.guestion.domain.model.Question;
 import com.mariusz.ideas.guestion.service.AnswerService;
 import com.mariusz.ideas.guestion.service.QuestionsService;
@@ -17,16 +18,19 @@ import java.util.UUID;
 public class QuestionViewController {
 
     private final QuestionsService questionsService;
+    private final CategoryService categoryService;
     private final AnswerService answerService;
 
-    public QuestionViewController(QuestionsService questionsService, AnswerService answerService) {
+    public QuestionViewController(QuestionsService questionsService, CategoryService categoryService, AnswerService answerService) {
         this.questionsService = questionsService;
+        this.categoryService = categoryService;
         this.answerService = answerService;
     }
 
     @GetMapping
     public String indexView(Model model){
         model.addAttribute("questions", questionsService.getQuestions());
+        model.addAttribute("categories", categoryService.getCategories());
 
         return "question/index";
     }
@@ -34,6 +38,7 @@ public class QuestionViewController {
     @GetMapping("{id}")
     public String singleView(Model model, @PathVariable UUID id){
         model.addAttribute("question", questionsService.getQuestion(id));
+        model.addAttribute("categories", categoryService.getCategories());
         model.addAttribute("answers", answerService.getAnswers(id));
 
         return "question/single";
