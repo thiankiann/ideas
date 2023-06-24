2cc18b729615bbe11f51d0f8465e2432f17a7fa6
@thiankiann <thiankiann@gmail.com>
2023-06-24 20:18:51 +0100

    Buttons  Edit prototype(admin/category/index.html) - logic added  ( editView()in CategoryAdminViewController)

--- src/main/java/com/mariusz/ideas/admin/controller/CategoryAdminViewController.java
+++ src/main/java/com/mariusz/ideas/admin/controller/CategoryAdminViewController.java
@@ -4,7 +4,10 @@ import com.mariusz.ideas.category.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -19,6 +22,14 @@ public class CategoryAdminViewController {
     @GetMapping
     public String indexView(Model model){
         model.addAttribute("categories", categoryService.getCategories());
+
         return "admin/category/index";
+    }
+
+    @GetMapping("{id}")
+    public String editView(Model model, @PathVariable UUID id){
+        model.addAttribute("categories", categoryService.getCategory(id));
+
+        return "admin/category/edit";
     }
 }
