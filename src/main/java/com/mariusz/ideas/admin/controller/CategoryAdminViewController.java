6162327865dbb583876cd53bd0adcfadcb72b478
@thiankiann <thiankiann@gmail.com>
2023-06-24 13:16:35 +0100

    tunning of our table ( to be continued )

--- src/main/java/com/mariusz/ideas/admin/controller/CategoryAdminViewController.java
+++ src/main/java/com/mariusz/ideas/admin/controller/CategoryAdminViewController.java
@@ -1,6 +1,8 @@
 package com.mariusz.ideas.admin.controller;
 
+import com.mariusz.ideas.category.service.CategoryService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/categories")
 public class CategoryAdminViewController {
 
+    private final CategoryService categoryService;
+
+    public CategoryAdminViewController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
     @GetMapping
-    public String indexView(){
+    public String indexView(Model model){
+        model.addAttribute("categories", categoryService.getCategories());
         return "admin/category/index";
     }
 }
