016633313e090c3785d69ffa63643f3a8773bd42
@thiankiann <thiankiann@gmail.com>
2023-06-24 23:35:08 +0100

    prototype Form (Quick Example) added to  edit.html

--- src/main/java/com/mariusz/ideas/admin/controller/CategoryAdminViewController.java
+++ src/main/java/com/mariusz/ideas/admin/controller/CategoryAdminViewController.java
@@ -4,7 +4,10 @@ import com.mariusz.ideas.category.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -20,5 +23,12 @@ public class CategoryAdminViewController {
     public String indexView(Model model){
         model.addAttribute("categories", categoryService.getCategories());
         return "admin/category/index";
+    }
+
+    @GetMapping("{id}")
+    public String editView(Model model, @PathVariable UUID id){
+        model.addAttribute("category", categoryService.getCategory(id));
+
+        return "admin/category/edit";
     }
 }
