d3b443f7430cb8f1c214ecd9151a80e06301bfea
@thiankiann <thiankiann@gmail.com>
2023-06-25 21:14:07 +0100

    postgres + database + Entity created

--- src/main/java/com/mariusz/ideas/category/domain/model/Category.java
+++ src/main/java/com/mariusz/ideas/category/domain/model/Category.java
@@ -1,9 +1,19 @@
 package com.mariusz.ideas.category.domain.model;
 
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
 import java.util.UUID;
 
+@Entity
+@Table(name = "categories" )
 public class Category {
 
+	@Id
 	private UUID id;
 
 	private String name;
