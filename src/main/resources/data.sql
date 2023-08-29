f37f9dd9a21970a9883c9960fc831eca7f4ac975
@thiankiann <thiankiann@gmail.com>
2023-08-29 21:51:14 +0100

    index.html

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -33,3 +33,20 @@ insert into questions (id, name, category_id) values
  insert into questions(id, name, category_id) values
  (gen_random_uuid(),'Why you should learn programing?', (select id from categories where name = 'Education')),
  (gen_random_uuid(),'Why Java is good for beginners?', (select id from categories where name = 'Education'));
+
+
+ insert into questions (id, name, category_id) values
+ (gen_random_uuid(), 'Jakie są najzdrowsze warzywa?', (select id from categories where name = 'Zdrowie'));
+
+ insert into answers (id, name, question_id) values
+ (gen_random_uuid(), 'Marchewka', (select id from questions where name = 'Jakie są najzdrowsze warzywa?')),
+ (gen_random_uuid(), 'Brokuł', (select id from questions where name = 'Jakie są najzdrowsze warzywa?')),
+ (gen_random_uuid(), 'Dynia', (select id from questions where name = 'Jakie są najzdrowsze warzywa?')),
+ (gen_random_uuid(), 'Groch', (select id from questions where name = 'Jakie są najzdrowsze warzywa?'));
+
+
+ insert into answers (id, name, question_id) values
+ (gen_random_uuid(), 'Gdańsk', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce')),
+ (gen_random_uuid(), 'Bieszczady', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce')),
+ (gen_random_uuid(), 'Mazury', (select id from questions where name = 'Gdzie najlepiej spędzić wakacje z Polsce'));
+
